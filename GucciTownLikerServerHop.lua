9c21cd044f7ad0441cc241f7247bc22183a552cc
Cynica <96399216+cynicamanee@users.noreply.github.com>
2022-05-29 14:27:59 +0200

    yeah update fix

--- GucciTownLikerServerHop.lua
+++ GucciTownLikerServerHop.lua
@@ -20,18 +20,18 @@ if game:IsLoaded() then
   end)
   task.wait(9)
   local x = {}
-        for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
-            if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
-                x[#x + 1] = v.id
-            end
-        end
-        if #x > 0 then
-            game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
-        else
-            return game.StarterGui:SetCore("SendNotification", {
-                Title = "CynicaHub",
-                Text = "Couldn't find a Server",
-                Duration = 4
-            })
-        end
+  for _, v in ipairs(game:GetService("HttpService"):JSONDecode(game:HttpGetAsync("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")).data) do
+    if type(v) == "table" and v.maxPlayers > v.playing and v.id ~= game.JobId then
+      x[#x + 1] = v.id
+    end
+  end
+  if #x > 0 then
+    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, x[math.random(1, #x)])
+  else
+    return game.StarterGui:SetCore("SendNotification", {
+       Title = "CynicaHub",
+       Text = "Couldn't find a Server",
+       Duration = 4
+     })
+  end
 end
